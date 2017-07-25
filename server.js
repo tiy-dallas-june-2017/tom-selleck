93c613d3d25e5529a8efb1d5b69b447843936a66
Eric Sowell <eric.sowell@gmail.com>
2017-07-25 13:52:29 -0500

    first template

--- server.js
+++ server.js
@@ -3,8 +3,19 @@ const mustache = require('mustache-express');
 
 const app = express();
 
+app.engine('mustache', mustache());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
 app.get('/', function(req, res) {
-  res.send('Put cool template stuff here instead.');
+  //res.send('Put cool template stuff here instead.');
+
+  let me = {
+    firstName: 'Nikki',
+    hairColor: 'Blond'
+  };
+
+  res.render('index', me);
 });
 
 
